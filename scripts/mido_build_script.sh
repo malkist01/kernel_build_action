861e53257240f6f36ffc7b7b07ca4511ef0ad099
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-01-20 12:21:30 +0700

    Update mido_build_script.sh

--- scripts/mido_build_script.sh
+++ scripts/mido_build_script.sh
@@ -9,8 +9,8 @@ clang() {
     rm -rf clang
     echo "Cloning clang"
     if [ ! -d "clang" ]; then
-        git clone -q https://gitlab.com/PixelOS-Devices/playgroundtc.git --depth=1 -b 17 clang
-        KBUILD_COMPILER_STRING="Cosmic clang 17.0"
+                git clone -q https://gitlab.com/GhostMaster69-dev/android-clang.git --depth=1 -b 19.0.1
+        KBUILD_COMPILER_STRING="Android clang 19.0.1"
         PATH="${PWD}/clang/bin:${PATH}"
     fi
     sudo apt install -y ccache
