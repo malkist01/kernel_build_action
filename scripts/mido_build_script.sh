813bb9a4d1fec23fa52649e2f200e37a8fc3398b
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-01-23 03:35:49 +0700

    Update mido_build_script.sh

--- scripts/mido_build_script.sh
+++ scripts/mido_build_script.sh
@@ -9,7 +9,7 @@ clang() {
     rm -rf clang
     echo "Cloning clang"
     if [ ! -d "clang" ]; then
-        git clone https://gitlab.com/LeCmnGend/clang.git -b neutron --depth=1 clang
+        git clone https://gitlab.com/LeCmnGend/clang.git -b clang-19 --depth=1 clang
         PATH="${PWD}/clang/bin:${PATH}"
     fi
     sudo apt install -y ccache
