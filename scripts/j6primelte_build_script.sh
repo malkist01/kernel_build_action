c71efe162d027302de6ec47307883070b0599c9e
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-02-09 15:25:49 -0500

    Add files via upload

--- scripts/j6primelte_build_script.sh
+++ scripts/j6primelte_build_script.sh
@@ -2,15 +2,15 @@
 
 # Dependencies
 rm -rf kernel
-git clone $REPO -b $BRANCH kernel
+git clone $REPO -b $BRANCH kernel 
 cd kernel
 
 clang() {
     rm -rf clang
     echo "Cloning clang"
     if [ ! -d "clang" ]; then
-        git clone https://github.com/khusika/canting_prebuilts_clang.git -b main --depth=1 clang
-        
+        git clone -q https://github.com/ZyCromerZ/Clang.git --depth=1 -b 20 clang
+        KBUILD_COMPILER_STRING="ZyCromerZ clang 20.0"
         PATH="${PWD}/clang/bin:${PATH}"
     fi
     sudo apt install -y ccache
@@ -18,6 +18,7 @@ clang() {
 }
 
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+IMAGE2=$(pwd)/out/arch/arm64/boot/dtbo.img
 DATE=$(date +"%Y%m%d-%H%M")
 START=$(date +"%s")
 KERNEL_DIR=$(pwd)
@@ -26,69 +27,41 @@ export CACHE
 export KBUILD_COMPILER_STRING
 ARCH=arm64
 export ARCH
-KBUILD_BUILD_HOST="android-server"
+KBUILD_BUILD_HOST="malkist"
 export KBUILD_BUILD_HOST
-KBUILD_BUILD_USER="malkist"
+KBUILD_BUILD_USER="android-server"
 export KBUILD_BUILD_USER
-DEVICE="Samsung Galaxy J6+"
+DEVICE="Redmi Note 4"
 export DEVICE
 CODENAME="j6primelte"
 export CODENAME
-# DEFCONFIG=""
+DEFCONFIG="teletubies_defconfig"
 export DEFCONFIG
-COMMIT_HASH=$(git log --oneline --pretty=tformat:"%h  %s  [%an]" --abbrev-commit --abbrev=1 -1)
+COMMIT_HASH=$(git rev-parse --short HEAD)
 export COMMIT_HASH
 PROCS=$(nproc --all)
 export PROCS
-STATUS=STABLE
+STATUS=TEST
 export STATUS
 source "${HOME}"/.bashrc && source "${HOME}"/.profile
-
+if [ $CACHE = 1 ]; then
+    ccache -M 100G
+    export USE_CCACHE=1
+fi
 LC_ALL=C
 export LC_ALL
-
-tg() {
-    curl -sX POST https://api.telegram.org/bot"${token}"/sendMessage -d chat_id="${chat_id}" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null
-}
-
-tgs() {
-    MD5=$(md5sum "$1" | cut -d' ' -f1)
-    curl -fsSL -X POST -F document=@"$1" https://api.telegram.org/bot"${token}"/sendDocument \
-        -F "chat_id=${chat_id}" \
-        -F "parse_mode=Markdown" \
-        -F "caption=$2 | *MD5*: \`$MD5\`"
-}
 
 # Send Build Info
 sendinfo() {
     tg "
-• IMcompiler Action •
+• sirCompiler Action •
 *Building on*: \`Github actions\`
 *Date*: \`${DATE}\`
 *Device*: \`${DEVICE} (${CODENAME})\`
 *Branch*: \`$(git rev-parse --abbrev-ref HEAD)\`
+*Last Commit*: [${COMMIT_HASH}](${REPO}/commit/${COMMIT_HASH})
 *Compiler*: \`${KBUILD_COMPILER_STRING}\`
-*Last Commit*: \`${COMMIT_HASH}\`
 *Build Status*: \`${STATUS}\`"
-}
-
-# Push kernel to channel
-push() {
-    cd AnyKernel || exit 1
-    ZIP=$(echo *.zip)
-    tgs "${ZIP}" "Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s). | For *${DEVICE} (${CODENAME})* | ${KBUILD_COMPILER_STRING}"
-}
-
-# Catch Error
-finderr() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d sticker="CAADBQADZwADqZrmFoa87YicX2hwAg" \
-        -d text="Build throw an error(s)"
-    error_sticker
-    exit 1
 }
 
 # Compile
@@ -98,25 +71,34 @@ compile() {
         rm -rf out && mkdir -p out
     fi
 
-    make O=out ARCH="${ARCH}" teletubies_defconfig
+    make O=out ARCH="${ARCH}" "${DEFCONFIG}"
     make -j"${PROCS}" O=out \
-        ARCH=$ARCH \
-        CC="clang" \
+        ARCH=arm64 \
+        LLVM=1 \
+        LLVM_IAS=1 \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        LD=ld.lld \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip \
+        CC=clang \
         CROSS_COMPILE=aarch64-linux-gnu- \
-        CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+        CROSS_COMPILE_ARM32=arm-linux-gnueabi
 
     if ! [ -a "$IMAGE" ]; then
-        finderr
         exit 1
     fi
 
-    git clone --depth=1 https://github.com/malkist01/anykernel.git AnyKernel -b master
+    git clone --depth=1 -b master https://github.com/RooGhz720/Anykernel3.git AnyKernel
     cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+    cp out/arch/arm64/boot/dtbo.img AnyKernel
+    cp out/arch/arm64/boot/dtb.img AnyKernel
 }
 # Zipping
 zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Kiwkiw-"${BRANCH}"-"${CODENAME}"-"${DATE}".zip ./*
+    zip -r9 Test-Build-Kernel-"${BRANCH}"-"${CODENAME}"-"${DATE}".zip ./*
     cd ..
 }
 
